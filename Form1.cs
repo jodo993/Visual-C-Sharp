64ad38222c37f8400e81537b5e5b634e1e4babe2
jodo993 <josephdo993@yahoo.com>
2016-11-04 17:03:28 -0700

    Chapter 7 Name Search Final

--- Form1.cs
+++ Form1.cs
@@ -7,8 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
-namespace JoeAutomotive
+namespace NameSearch
 {
     public partial class Form1 : Form
     {
@@ -17,186 +18,137 @@ namespace JoeAutomotive
             InitializeComponent();
         }
 
-        // Value-returning methods needed:
-        // oilLubeCharges, flushCharges, miscCharges, otherCharges, taxCharges, totalCharges
-
-        // Checks the oil and lube and assign a price to it if selected
-        private double OilLubeCharges(out double oilLubeCost)
+        private int findBoyName(string nameOfBoy, string[] boyNames)
         {
-            double oilChange = 26;      // Cost of oil change
-            double lubeJob = 18;        // Cost of lube job
+            bool found = false;                             // Set found to false until/if name is found
+            int i = 0;                                      // Start at sub of zero in array
+            int position = -1;                              // Position determines which if statement in checkButton uses
 
-            // Determines how much to charge for oil and lube
-            if (oilChangeCheckBox.Checked && lubeJobCheckBox.Checked)
-                oilLubeCost = oilChange + lubeJob;
-            else if (oilChangeCheckBox.Checked)
-                oilLubeCost = oilChange;
-            else if (lubeJobCheckBox.Checked)
-                oilLubeCost = lubeJob;
-            else
-                oilLubeCost = 0;
-            return oilLubeCost;
+            // Look for name with sequential search
+            while (!found && i < boyNames.Length)           
+            {
+                if (nameOfBoy == boyNames[i])
+                {
+                    found = true;
+                    position = i;
+                }
+                i++;
+            }
+            return position;
         }
 
-        // Checks the flushes and assign a price to it if selected
-        private double FlushCharges (out double flushCost)
+        private int findGirlName(string nameOfGirl, string[] girlNames)
         {
-            double radiatorFlush = 30;          // Cost of radiator flush
-            double transmissionFlush = 80;      // Cost of transmission flush
+            bool found = false;                             // Set found to false until/if name is found
+            int i = 0;                                      // Start at sub of zero in array
+            int position = -1;                              // Position determines which if statement in checkButton uses
 
-            // Determines how much to charge for flush cost
-            if (radiatorFlushCheckBox.Checked && transmissionFlushCheckBox.Checked)
-                flushCost = radiatorFlush + transmissionFlush;
-            else if (radiatorFlushCheckBox.Checked)
-                flushCost = radiatorFlush;
-            else if (transmissionFlushCheckBox.Checked)
-                flushCost = transmissionFlush;
-            else
-                flushCost = 0;
-            return flushCost;
+            // Look for name with sequential search
+            while (!found && i < girlNames.Length)
+            {
+                if (nameOfGirl == girlNames[i])
+                {
+                    found = true;
+                    position = i;
+                }
+                i++;
+            }
+            return position;
         }
 
-        // Check the misc and assign a price to it if selected
-        private double MiscCharges (out double miscCost)
-        {
-            double inspection = 15;         // Cost of inspection
-            double replaceMuffler = 100;    // Cost to replace muffler
-            double tireRotation = 20;       // Cost to rotate tires
-
-            // Determines how much to charge for misc items
-            if (inspectionCheckBox.Checked && replaceMufflerCheckBox.Checked && tireRotationCheckBox.Checked)
-                miscCost = inspection + replaceMuffler + tireRotation;
-            else if (inspectionCheckBox.Checked && replaceMufflerCheckBox.Checked)
-                miscCost = inspection + replaceMuffler;
-            else if (inspectionCheckBox.Checked && tireRotationCheckBox.Checked)
-                miscCost = inspection + tireRotation;
-            else if (replaceMufflerCheckBox.Checked && tireRotationCheckBox.Checked)
-                miscCost = replaceMuffler + tireRotation;
-            else if (inspectionCheckBox.Checked)
-                miscCost = inspection;
-            else if (replaceMufflerCheckBox.Checked)
-                miscCost = replaceMuffler;
-            else if (tireRotationCheckBox.Checked)
-                miscCost = tireRotation;
-            else
-                miscCost = 0;
-            return miscCost;
-        }
-
-        // Check the other and assign a price to it if selected
-        private double OtherCharges ()
-        {
-            double parts;               // Cost of parts user entered
-            double labor;               // Cost of labor user entered
-            double partCost = 0;
-            double laborCost = 0;
-
-            // Convert string of parts/labor cost into double
-            if (double.TryParse(partsTextBox.Text, out parts))
-                partCost = parts;
-            if (double.TryParse(laborTextBox.Text, out labor))
-                laborCost = labor;
-            return partCost + laborCost;
-        }
-
-        // Finds the tax cost for parts only
-        private double TaxCharges (ref double otherCost)
-        {
-            // otherCost is partCost 
-            double saleTax = 0.06;                  // Sale tax percentage
-            double tax = otherCost * saleTax;       // Finding the tax
-            return tax;
-        }
-
-        // Find cost of entire services, parts, and tax
-        private double TotalCharges (ref double oilLubeCharge, ref double flushCharges, ref double miscCharges, ref double otherCharges, ref double taxCharges)
-        {
-            double totalCost;
-
-            totalCost = oilLubeCharge + flushCharges + miscCharges + otherCharges + taxCharges;
-            return totalCost;
-        }
-
-        // All void clears all boxes, reset program
-        private void ClearOilLube()
-        {
-            oilChangeCheckBox.Checked = false;
-            lubeJobCheckBox.Checked = false;
-        }
-
-        private void ClearFlushes()
-        {
-            radiatorFlushCheckBox.Checked = false;
-            transmissionFlushCheckBox.Checked = false;
-        }
-
-        private void ClearMisc()
-        {
-            inspectionCheckBox.Checked = false;
-            replaceMufflerCheckBox.Checked = false;
-            tireRotationCheckBox.Checked = false;
-        }
-
-        private void ClearOther()
-        {
-            partsTextBox.Text = "";
-            laborTextBox.Text = "";
-        }
-
-        private void ClearFees()
-        {
-            serviceAndLaborLabel.Text = "";
-            partsLabel.Text = "";
-            taxLabel.Text = "";
-            totalFeeLabel.Text = "";
-        }
-           
-        // Calls other methods
-        private void calculateButton_Click(object sender, EventArgs e)
+        private void checkButton_Click(object sender, EventArgs e)
         {
             try
             {
-                // Variables to hold the cost of each charges
-                double oilLubeCharges,
-                       oilLubeCost,
-                       flushCharges,
-                       flushCost,
-                       miscCharges,
-                       miscCost,
-                       otherCharges,
-                       taxCharges,
-                       totalCharges;
+                // Create an empty array with size of 200
+                const int SIZE = 200;
+                string[] boyNames = new string[SIZE];
+                string[] girlNames = new string[SIZE];
 
-                double parts,
-                       partCharge,
-                       labor,
-                       laborCharge;
+                // User input of a boy name and a girl name
+                string nameOfBoy;
+                string nameOfGirl;
 
-                // Find the amount spent on parts in text box
-                if (double.TryParse(partsTextBox.Text, out parts))
-                    partCharge = parts;
-                else
-                    partCharge = 0;
+                // Retrieve a boy name and a girl name from the text box
+                nameOfBoy = boyNameTextBox.Text;
+                nameOfGirl = girlNameTextBox.Text;
 
-                // Find the amount spent on labor in text box
-                if (double.TryParse(laborTextBox.Text, out labor))
-                    laborCharge = labor;
-                else
-                    laborCharge = 0;
+                // Declaring streamreader variable
+                StreamReader inputFileBoys;
+                StreamReader inputFIleGirls;
 
-                // Calling methods to find cost for respective sections
-                oilLubeCharges = OilLubeCharges(out oilLubeCost);
-                flushCharges = FlushCharges(out flushCost);
-                miscCharges = MiscCharges(out miscCost);
-                otherCharges = OtherCharges();
-                taxCharges = TaxCharges(ref partCharge);
-                totalCharges = TotalCharges(ref oilLubeCharges, ref flushCharges, ref miscCharges, ref otherCharges, ref taxCharges);
+                // Open text files
+                inputFileBoys = File.OpenText("BoyNames.txt");
+                inputFIleGirls = File.OpenText("GirlNames.txt");
 
-                // Display results
-                serviceAndLaborLabel.Text = (oilLubeCharges + flushCharges + miscCharges + laborCharge).ToString("C");
-                partsLabel.Text = partCharge.ToString("C");
-                taxLabel.Text = taxCharges.ToString("C");
-                totalFeeLabel.Text = totalCharges.ToString("C");
+                // Read boy names and put in list
+                int indexBoy = 0;
+                while (indexBoy < boyNames.Length && !inputFileBoys.EndOfStream)
+                {
+                    boyNames[indexBoy] = inputFileBoys.ReadLine();
+                    indexBoy++;
+                }
+
+                // Read girl names and put in list
+                int indexGirl = 0;
+                while (indexGirl < girlNames.Length && !inputFIleGirls.EndOfStream)
+                {
+                    girlNames[indexGirl] = inputFIleGirls.ReadLine();
+                    indexGirl++;
+                }
+
+                // Closes text file
+                inputFileBoys.Close();
+                inputFIleGirls.Close();
+
+                // If both boy name and girl name are entered
+                // Determines if both are popular or only one is popular and the other is not
+                if (nameOfBoy != "" && nameOfGirl != "")
+                {
+                    if (findBoyName(nameOfBoy, boyNames) != -1 && findGirlName(nameOfGirl, girlNames) != -1)
+                    {
+                        resultLabel.Text = "Both " + nameOfBoy + " and " + nameOfGirl + " are among most popular.";
+                    }
+                    else if (findBoyName(nameOfBoy, boyNames) != -1 && findGirlName(nameOfGirl, girlNames) == -1)
+                    {
+                        resultLabel.Text = nameOfBoy + " is among most popular, but " + nameOfGirl + " is not.";
+                    }
+                    else if (findGirlName(nameOfGirl, girlNames) != -1 && findBoyName(nameOfBoy, boyNames) == -1)
+                    {
+                        resultLabel.Text = nameOfGirl + " is among most popular, but " + nameOfBoy + " is not.";
+                    }
+                    else
+                    {
+                        resultLabel.Text = "Both are not among most popular.";
+                    }
+                } // If boy name is entered but girl name is not
+                else if (nameOfBoy != "" && nameOfGirl == "")
+                {
+                    if (findBoyName(nameOfBoy, boyNames) != -1)
+                    {
+                        resultLabel.Text = nameOfBoy + " is among most popular.";
+                    }
+                    else
+                    {
+                        resultLabel.Text = nameOfBoy + " is not among most popular.";
+                    }
+
+                } // If girl name is entered but boy name is not
+                else if (nameOfGirl != "" & nameOfBoy == "")
+                {
+                    if (findGirlName(nameOfGirl, girlNames) != -1)
+                    {
+                        resultLabel.Text = nameOfGirl + " is among most popular.";
+                    }
+                    else
+                    {
+                        resultLabel.Text = nameOfGirl + " is not among most popular.";
+                    }
+                }
+                else // If no names were entered
+                {
+                    resultLabel.Text = "Nothing was entered.";
+                }
             }
 
             catch (Exception ex)
@@ -205,19 +157,16 @@ namespace JoeAutomotive
             }
         }
 
-        // Calls other methods to clear the program boxes
         private void clearButton_Click(object sender, EventArgs e)
         {
-            ClearOilLube();
-            ClearFlushes();
-            ClearMisc();
-            ClearOther();
-            ClearFees();
+            // Clear the textboxes
+            boyNameTextBox.Text = "";
+            girlNameTextBox.Text = "";
         }
 
-        // Close the program
         private void exitButton_Click(object sender, EventArgs e)
         {
+            // Close the program
             this.Close();
         }
     }
